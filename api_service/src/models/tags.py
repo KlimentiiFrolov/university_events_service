98cf58be0af2ac8745fdd2f5c02dc9f07260b381
Gealor <gzazulya@gmail.com>
2026-09-18 21:10:58 +0400

    add tags relationship

--- api_service/src/models/tags.py
+++ api_service/src/models/tags.py
@@ -1,14 +1,20 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import CheckConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins.id_pk_mixin import IdPrimaryKeyMixin
 
+if TYPE_CHECKING:
+    from .event_tags import EventTag
 
-class Tags(Base, IdPrimaryKeyMixin):
+class Tag(Base, IdPrimaryKeyMixin):
     __tablename__ = "tags"
 
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+    events: Mapped[list["EventTag"]] = relationship(back_populates="tag")
 
     __table_args__ = (
         CheckConstraint('LENGTH(name)>2', name="ck_name_length_gt_2"),
